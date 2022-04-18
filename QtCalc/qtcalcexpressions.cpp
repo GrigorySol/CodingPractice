20eb9f6e9843adbf2be3b1cc3620b79c39f0bb0c
Grigory Solovyov <nuclearec@Grigorys-MacBook-Pro.local>
2022-04-18 14:31:44 +0300

    Added expression module for external calculations
    An attempt to divide the program into simple blocks

---
+++ QtCalc/qtcalcexpressions.cpp
@@ -0,0 +1,24 @@
+double plus(double &x, double &y)
+{
+    return x + y;
+};
+
+double minus(double &x, double &y)
+{
+    return x - y;
+};
+
+double mult(double &x, double &y)
+{
+    return x * y;
+};
+
+double devide(double &x, double &y)
+{
+    return x / y;
+};
+
+double percent(double &x, double &y)
+{
+    return x / 100 * y;
+};
---
+++ QtCalc/qtcalcexpressions.h
@@ -0,0 +1,15 @@
+#ifndef QTCALCEXPRESSIONS_H
+#define QTCALCEXPRESSIONS_H
+
+double plus(double &x, double &y);
+
+double minus(double &x, double &y);
+
+double mult(double &x, double &y);
+
+double devide(double &x, double &y);
+
+double percent(double &x, double &y);
+
+
+#endif // QTCALCEXPRESSIONS_H
